4312cc66fbf9f91fb74e8df9575e3c55b6ce7626
unknown <khaleelibraheem05@gmail.com>
2024-08-20 02:55:26 -0400

    Header Styling

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import "../styles/Footer.scss";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        © <span>TAV college's Car Wash</span>, All Right Reserved.
+      </p>
+    </footer>
+  );
+}
